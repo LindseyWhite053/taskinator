ec4a329e2db81bb2f29275f55514ceefba6d7d7e
Lindsey White <LindseyWhite053@Gmail.com>
2022-04-19 20:12:09 -0400

    add ability to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,15 @@ var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    
+
+    //check if input values are empty strings
+    if (taskNameInput ===""|| taskTypeInput === "") {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+formEl.reset();
+
     //package up data as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -15,9 +23,10 @@ var taskFormHandler = function(event) {
 
     //send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
-}
 
-var createTaskEl = function(taskDataObj) {
+};
+
+var createTaskEl = function (taskDataObj) {
 // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -25,7 +34,7 @@ var createTaskEl = function(taskDataObj) {
 //create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
  
  
     listItemEl.appendChild(taskInfoEl);
